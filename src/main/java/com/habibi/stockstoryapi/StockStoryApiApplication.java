3d0546fbcc9b517cda93e3a47a4875a0a9312d43
하지훈 <jnh03336@gmail.com>
2023-09-23 21:49:52 +0700

    initial commit

---
+++ src/main/java/com/habibi/stockstoryapi/StockStoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.habibi.stockstoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockStoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockStoryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habibi/stockstoryapi/StockStoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habibi.stockstoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockStoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
