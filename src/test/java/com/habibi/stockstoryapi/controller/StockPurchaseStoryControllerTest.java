55fda98b79af2cce81c7eede2d6c8af1bf95c97b
하지훈 <jnh03336@gmail.com>
2023-10-01 09:09:47 +0700

    test: update to pass body payload

--- src/test/java/com/habibi/stockstoryapi/controller/StockPurchaseStoryControllerTest.java
+++ src/test/java/com/habibi/stockstoryapi/controller/StockPurchaseStoryControllerTest.java
@@ -1,11 +1,15 @@
 package com.habibi.stockstoryapi.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.habibi.stockstoryapi.dto.CreateStockStoryDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -14,13 +18,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class StockPurchaseStoryControllerTest {
 
     @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
     MockMvc mockMvc;
 
     @Test
     public void createStockPurchaseStory() throws Exception {
+        CreateStockStoryDto dto = CreateStockStoryDto.builder()
+                .stockCode("105560")
+                .dt(LocalDateTime.of(2023, 10, 1, 15, 00))
+                .stockPrices(new int[]{55300, 55100, 56000})
+                .story("PBR이 지나치게 낮아져서 오를 여지가 있다고 생각함.")
+                .build();
+
         mockMvc.perform(post("/api/stock-purchase-stories")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
                 )
                 .andExpect(status().isCreated());
     }
--- src/test/java/com/habibi/stockstoryapi/controller/StockSellStoryControllerTest.java
+++ src/test/java/com/habibi/stockstoryapi/controller/StockSellStoryControllerTest.java
@@ -1,11 +1,15 @@
 package com.habibi.stockstoryapi.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.habibi.stockstoryapi.dto.CreateStockStoryDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -14,13 +18,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class StockSellStoryControllerTest {
 
     @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
     MockMvc mockMvc;
 
     @Test
     public void createStockSellStory() throws Exception {
+        CreateStockStoryDto dto = CreateStockStoryDto.builder()
+                .stockCode("035720")
+                .stockPrices(new int[]{43950, 44000, 43900})
+                .dt(LocalDateTime.of(2023, 10, 1, 15, 00))
+                .story("실적이 안 좋고, 개선 여지가 크게 보이지 않아 매도")
+                .build();
+
         mockMvc.perform(post("/api/stock-sell-stories")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(dto))
                 )
                 .andExpect(status().isCreated());
     }
