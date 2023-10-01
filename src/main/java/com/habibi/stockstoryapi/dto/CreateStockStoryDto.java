9dda22b92fc61001766dd7bbb44a9497584602a6
하지훈 <jnh03336@gmail.com>
2023-10-01 09:08:57 +0700

    feat: add dto for create stock story (use for both purchase and sell)

---
+++ src/main/java/com/habibi/stockstoryapi/dto/CreateStockStoryDto.java
@@ -0,0 +1,16 @@
+package com.habibi.stockstoryapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class CreateStockStoryDto {
+    private String stockCode;
+    private int[] stockPrices;
+    private LocalDateTime dt;
+    private String story;
+}
