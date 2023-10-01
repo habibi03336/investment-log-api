ab12f29c60d51d678b1978ddf586a0b113b97ac7
하지훈 <jnh03336@gmail.com>
2023-10-01 09:11:05 +0700

    feat: update to receieve dto as a request body

--- src/main/java/com/habibi/stockstoryapi/controller/StockSellStoryController.java
+++ src/main/java/com/habibi/stockstoryapi/controller/StockSellStoryController.java
@@ -1,27 +1,24 @@
 package com.habibi.stockstoryapi.controller;
 
+import com.habibi.stockstoryapi.dto.CreateStockStoryDto;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Controller
 @RequestMapping(value="/api/stock-sell-stories", produces = MediaTypes.HAL_JSON_VALUE)
 public class StockSellStoryController {
 
     @PostMapping
-    public ResponseEntity createStockSellStory(){
-        URI createdUri = linkTo(methodOn(StockSellStoryController.class)
-                .createStockSellStory())
-                .toUri();
+    public ResponseEntity createStockSellStory(@RequestBody CreateStockStoryDto createStockSellStoryDto){
+        URI createdUri = linkTo(StockSellStoryController.class).slash("{id}").toUri();
         return ResponseEntity.created(createdUri).build();
     }
-
 }
