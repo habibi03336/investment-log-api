8aed70e5b4cc62150e89683a6607a7d74eb002c5
하지훈 <jnh03336@gmail.com>
2023-10-08 09:38:32 +0700

    feat: add more jpa operations in need

--- src/main/java/com/habibi/stockstoryapi/repository/StockPurchaseRecordRepository.java
+++ src/main/java/com/habibi/stockstoryapi/repository/StockPurchaseRecordRepository.java
@@ -4,5 +4,12 @@ package com.habibi.stockstoryapi.repository;
 import com.habibi.stockstoryapi.domain.StockPurchaseRecordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface StockPurchaseRecordRepository extends JpaRepository<StockPurchaseRecordEntity, Long> {
+
+    List<StockPurchaseRecordEntity> findAllByStockCode(String stockCode);
+    List<StockPurchaseRecordEntity> findAllByPurchaseDtIsBefore(LocalDate date);
+    List<StockPurchaseRecordEntity> findAllByPurchaseDtIsBetween(LocalDate start, LocalDate end);
 }
--- src/main/java/com/habibi/stockstoryapi/repository/StockSellRecordRepository.java
+++ src/main/java/com/habibi/stockstoryapi/repository/StockSellRecordRepository.java
@@ -1,7 +1,15 @@
 package com.habibi.stockstoryapi.repository;
 
+
 import com.habibi.stockstoryapi.domain.StockSellRecordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface StockSellRecordRepository extends JpaRepository<StockSellRecordEntity, Long> {
+
+    List<StockSellRecordEntity> findAllByStockCode(String stockCode);
+    List<StockSellRecordEntity> findAllBySellDtIsBefore(LocalDate date);
+    List<StockSellRecordEntity> findAllBySellDtIsBetween(LocalDate start, LocalDate end);
 }
