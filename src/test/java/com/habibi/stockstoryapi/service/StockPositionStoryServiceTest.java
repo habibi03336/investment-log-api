52e07ea74595b6f08ce0f18f0fdd8a89d43447e0
하지훈 <jnh03336@gmail.com>
2023-10-22 17:22:36 +0700

    feat: update StockPositionStory test
    - update according to the StockPositionStoryDto data type & meaning change
    - update according to the method definition of StockPositionStoryRepository

--- src/test/java/com/habibi/stockstoryapi/service/StockPositionStoryServiceTest.java
+++ src/test/java/com/habibi/stockstoryapi/service/StockPositionStoryServiceTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -29,19 +29,22 @@ public class StockPositionStoryServiceTest {
         stockPositionStoryRepository = Mockito.mock(StockPositionStoryRepository.class);
         stockPurchaseRecordRepository = Mockito.mock(StockPurchaseRecordRepository.class);
         stockSellRecordRepository = Mockito.mock(StockSellRecordRepository.class);
-        stockPositionStoryService = null;
+        stockPositionStoryService = new StockPositionStoryServiceImpl(stockPurchaseRecordRepository, stockSellRecordRepository, stockPositionStoryRepository);
     }
     @Test
     public void readStockLongPositionStoryOfCertainStock(){
         // given
         String stockCode = "035420";
+        // same storyId -> same stock purchase/sell date
         long storyId1 = 1;
+        LocalDate stockPurchaseDate1 = LocalDate.of(2023, 10, 10);
         List<StockPurchaseRecordEntity> stockPurchaseRecordEntities = new ArrayList<>();
         stockPurchaseRecordEntities.add(StockPurchaseRecordEntity
                 .builder()
                 .stockCode(stockCode)
                 .purchasePrice(50000)
                 .storyId(storyId1)
+                .purchaseDt(stockPurchaseDate1)
                 .build()
         );
         stockPurchaseRecordEntities.add(StockPurchaseRecordEntity
@@ -49,38 +52,33 @@ public class StockPositionStoryServiceTest {
                 .stockCode(stockCode)
                 .purchasePrice(50100)
                 .storyId(storyId1)
+                .purchaseDt(stockPurchaseDate1)
                 .build()
         );
         long storyId2 = 2;
+        LocalDate stockPurchaseDate2 = LocalDate.of(2023, 10, 23);
         stockPurchaseRecordEntities.add(StockPurchaseRecordEntity
                 .builder()
                 .stockCode(stockCode)
                 .purchasePrice(60000)
                 .storyId(storyId2)
+                .purchaseDt(stockPurchaseDate2)
                 .build()
         );
         when(stockPurchaseRecordRepository.findAllByStockCode(stockCode)).thenReturn(stockPurchaseRecordEntities);
-        LocalDateTime creationDt1 = LocalDateTime.of(2023, 10, 21, 0, 0, 0);
         String story1 = "Naver is great company";
-        when(stockPositionStoryRepository.findById(storyId1)).thenReturn(
-                Optional.ofNullable(
-                        StockPositionStoryEntity.builder()
-                                .storyId(storyId1)
-                                .content(story1)
-                                .creationDt(creationDt1)
-                                .build()
-                )
+        when(stockPositionStoryRepository.findByStoryId(storyId1)).thenReturn(
+            StockPositionStoryEntity.builder()
+                    .storyId(storyId1)
+                    .content(story1)
+                    .build()
         );
-        LocalDateTime creationDt2 = LocalDateTime.of(2023, 10, 23, 0, 0, 0);
         String story2 = "Naver has great potential";
-        when(stockPositionStoryRepository.findById(storyId2)).thenReturn(
-                Optional.ofNullable(
-                        StockPositionStoryEntity.builder()
-                                .storyId(storyId2)
-                                .content(story2)
-                                .creationDt(creationDt2)
-                                .build()
-                )
+        when(stockPositionStoryRepository.findByStoryId(storyId2)).thenReturn(
+            StockPositionStoryEntity.builder()
+                    .storyId(storyId2)
+                    .content(story2)
+                    .build()
         );
 
         // when
@@ -95,7 +93,7 @@ public class StockPositionStoryServiceTest {
         assertThat(stockPositionStoryDtoOfStoryId2.getStockPrices().length).isEqualTo(1);
         assertThat(stockPositionStoryDtoOfStoryId2.getStockPrices()[0]).isEqualTo(60000);
         assertThat(stockPositionStoryDtoOfStoryId2.getStory()).isEqualTo(story2);
-        assertThat(stockPositionStoryDtoOfStoryId2.getDt()).isEqualTo(creationDt2);
+        assertThat(stockPositionStoryDtoOfStoryId2.getDt()).isEqualTo(stockPurchaseDate2);
 
         StockPositionStoryDto stockPositionStoryDtoOfStoryId1 = stockPositionStoryDtos.get(1);
         assertThat(stockPositionStoryDtoOfStoryId1.getStockCode()).isEqualTo(stockCode);
@@ -103,27 +101,31 @@ public class StockPositionStoryServiceTest {
         assertThat(stockPositionStoryDtoOfStoryId1.getStockPrices()).contains(50000);
         assertThat(stockPositionStoryDtoOfStoryId1.getStockPrices()).contains(50100);
         assertThat(stockPositionStoryDtoOfStoryId1.getStory()).isEqualTo(story1);
-        assertThat(stockPositionStoryDtoOfStoryId1.getDt()).isEqualTo(creationDt1);
+        assertThat(stockPositionStoryDtoOfStoryId1.getDt()).isEqualTo(stockPurchaseDate1);
     }
     @Test
     public void readStockShortPositionStoryOfCertainStock(){
         // given
         String stockCode = "035420";
         long storyId1 = 1;
+        LocalDate stockSellDate1 = LocalDate.of(2023, 10, 21);
         List<StockSellRecordEntity> stockSellRecordEntities = new ArrayList<>();
         stockSellRecordEntities.add(StockSellRecordEntity
                 .builder()
                 .stockCode(stockCode)
                 .sellPrice(60000)
                 .storyId(storyId1)
+                .sellDt(stockSellDate1)
                 .build()
         );
         long storyId2 = 2;
+        LocalDate stockSellDate2 = LocalDate.of(2023, 10, 22);
         stockSellRecordEntities.add(StockSellRecordEntity
                 .builder()
                 .stockCode(stockCode)
                 .sellPrice(78000)
                 .storyId(storyId2)
+                .sellDt(stockSellDate2)
                 .build()
         );
         stockSellRecordEntities.add(StockSellRecordEntity
@@ -131,29 +133,25 @@ public class StockPositionStoryServiceTest {
                 .stockCode(stockCode)
                 .sellPrice(81000)
                 .storyId(storyId2)
+                .sellDt(stockSellDate2)
                 .build()
         );
         when(stockSellRecordRepository.findAllByStockCode(stockCode)).thenReturn(stockSellRecordEntities);
-
         String story1 = "IT market is too much hyped";
-        LocalDateTime creationDt1 = LocalDateTime.of(2023, 10, 21, 0, 0, 0);
         when(stockPositionStoryRepository.findById(storyId1)).thenReturn(
                 Optional.ofNullable(
                         StockPositionStoryEntity.builder()
                                 .storyId(storyId1)
                                 .content(story1)
-                                .creationDt(creationDt1)
                                 .build()
                 )
         );
         String story2 = "IT market is way too much hyped";
-        LocalDateTime creationDt2 = LocalDateTime.of(2023, 10, 23, 0, 0, 0);
         when(stockPositionStoryRepository.findById(storyId2)).thenReturn(
                 Optional.ofNullable(
                         StockPositionStoryEntity.builder()
                                 .storyId(storyId2)
                                 .content(story2)
-                                .creationDt(creationDt2)
                                 .build()
                 )
         );
@@ -170,13 +168,13 @@ public class StockPositionStoryServiceTest {
         assertThat(stockPositionStoryDtoOfStoryId2.getStockPrices()).contains(78000);
         assertThat(stockPositionStoryDtoOfStoryId2.getStockPrices()).contains(81000);
         assertThat(stockPositionStoryDtoOfStoryId2.getStory()).isEqualTo(story2);
-        assertThat(stockPositionStoryDtoOfStoryId2.getDt()).isEqualTo(creationDt2);
+        assertThat(stockPositionStoryDtoOfStoryId2.getDt()).isEqualTo(stockSellDate2);
         
         StockPositionStoryDto stockPositionStoryDtoOfStoryId1 = stockPositionStoryDtos.get(1);
         assertThat(stockPositionStoryDtoOfStoryId1.getStockCode()).isEqualTo(stockCode);
         assertThat(stockPositionStoryDtoOfStoryId1.getStockPrices().length).isEqualTo(1);
         assertThat(stockPositionStoryDtoOfStoryId1.getStockPrices()[0]).isEqualTo(60000);
         assertThat(stockPositionStoryDtoOfStoryId1.getStory()).isEqualTo(story1);
-        assertThat(stockPositionStoryDtoOfStoryId1.getDt()).isEqualTo(creationDt1);
+        assertThat(stockPositionStoryDtoOfStoryId1.getDt()).isEqualTo(stockSellDate1);
     }
 }
