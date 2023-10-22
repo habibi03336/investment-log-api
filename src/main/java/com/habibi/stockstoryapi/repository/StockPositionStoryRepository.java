0183a6f36a75bff3b0fbe74455016bda56c2786f
하지훈 <jnh03336@gmail.com>
2023-10-22 17:21:23 +0700

    feat: define repository method

--- src/main/java/com/habibi/stockstoryapi/repository/StockPositionStoryRepository.java
+++ src/main/java/com/habibi/stockstoryapi/repository/StockPositionStoryRepository.java
@@ -4,4 +4,5 @@ import com.habibi.stockstoryapi.domain.StockPositionStoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StockPositionStoryRepository extends JpaRepository<StockPositionStoryEntity, Long> {
+    StockPositionStoryEntity findByStoryId(long storyId);
 }
