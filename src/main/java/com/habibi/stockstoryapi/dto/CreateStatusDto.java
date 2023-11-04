713d7e26d3845d0963c102e1ca21e72ddd3d766a
하지훈 <jnh03336@gmail.com>
2023-11-04 14:02:21 +0700

    feat: define properties to commute create status

--- src/main/java/com/habibi/stockstoryapi/dto/CreateStatusDto.java
+++ src/main/java/com/habibi/stockstoryapi/dto/CreateStatusDto.java
@@ -1,4 +1,16 @@
 package com.habibi.stockstoryapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
 public class CreateStatusDto {
+    private Status status;
+
+    public enum Status {
+        SUCCESS,
+        FAIL
+    }
 }
