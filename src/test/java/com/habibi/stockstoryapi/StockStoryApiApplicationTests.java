de23283dd4d875f99503b354e3d2c2b96b19af60
하지훈 <jnh03336@gmail.com>
2024-01-03 21:49:10 +0700

    test: 주식 판매 이야기 등록 후 조회 API 테스트

--- src/test/java/com/habibi/stockstoryapi/StockStoryApiApplicationTests.java
+++ src/test/java/com/habibi/stockstoryapi/StockStoryApiApplicationTests.java
@@ -126,4 +126,86 @@ class StockStoryApiApplicationTests {
 				)
 		);
 	}
+
+	@Test
+	public void 주식판매이야기등록후조회API테스트() throws Exception {
+		// 주식 구매 이야기 등록
+		ResultActions responseOfCreateLongStory = mockMvc.perform(
+				MockMvcRequestBuilders
+						.post("/api/stock-story")
+						.contentType("application/json")
+						.content("""
+							{
+							  "long": true,
+							  "stockCode": "066570",
+							  "stockPrices": [30000, 35000, 37000, 40000, 41100, 45600, 50000],
+							  "dt": "2024-01-02",
+							  "story": "주식 구매 이야기 등록"
+							}
+						""")
+		);
+		responseOfCreateLongStory.andExpect(MockMvcResultMatchers.status().isCreated());
+
+		// 주식 판매 이야기 등록
+		ResultActions responseOfCreateShortStory = mockMvc.perform(
+				MockMvcRequestBuilders
+						.post("/api/stock-story")
+						.contentType("application/json")
+						.content("""
+							{
+							  "long": false,
+							  "stockCode": "066570",
+							  "stockPrices": [67100, 68500, 65100, 68000],
+							  "dt": "2024-01-03",
+							  "story": "주식 판매 이야기 등록"
+							}
+						""")
+		);
+		responseOfCreateShortStory.andExpect(MockMvcResultMatchers.status().isCreated());
+
+		// 현재 소유한 주식 조회
+		ResultActions responseOfOwnStock = mockMvc.perform(
+				MockMvcRequestBuilders
+						.get("/api/own-stock")
+						.contentType("application/json")
+		);
+
+		responseOfOwnStock.andExpect(MockMvcResultMatchers.jsonPath("$[0].stockName", CoreMatchers.is("LG전자")));
+		responseOfOwnStock.andExpect(MockMvcResultMatchers.jsonPath("$[0].stockCode", CoreMatchers.is("066570")));
+		responseOfOwnStock.andExpect(MockMvcResultMatchers.jsonPath("$[0].stockCount", CoreMatchers.is(3)));
+		responseOfOwnStock.andExpect(MockMvcResultMatchers.jsonPath("$[0].averagePurchasePrice", CoreMatchers.is(39814)));
+
+		// 실현된 주식 투자를 조회
+		ResultActions responseOfRealizedStock = mockMvc.perform(
+				MockMvcRequestBuilders
+						.get("/api/realized-stock")
+						.contentType("application/json")
+		);
+
+		responseOfRealizedStock.andExpect(MockMvcResultMatchers.jsonPath("$[0].stockName", CoreMatchers.is("LG전자")));
+		responseOfRealizedStock.andExpect(MockMvcResultMatchers.jsonPath("$[0].stockCode", CoreMatchers.is("066570")));
+		responseOfRealizedStock.andExpect(MockMvcResultMatchers.jsonPath("$[0].stockCount", CoreMatchers.is(4)));
+		responseOfRealizedStock.andExpect(MockMvcResultMatchers.jsonPath("$[0].averageSellPrice", CoreMatchers.is(67175)));
+		responseOfRealizedStock.andExpect(MockMvcResultMatchers.jsonPath("$[0].averagePurchasePrice", CoreMatchers.is(39814)));
+
+
+		// 특정 기간의 모든 판매 기록 반환 조회 (끝나는 날짜까지 포함하여 판매 기록 반환)
+		ResultActions responseOfAllPurchaseRecordOnPeriod= mockMvc.perform(
+				MockMvcRequestBuilders
+						.get("/api/stock-sell-record?start-period=2024-01-01&end-period=2024-01-03")
+						.contentType("application/json")
+		);
+
+		responseOfAllPurchaseRecordOnPeriod.andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(4)));
+		responseOfAllPurchaseRecordOnPeriod.andExpect(MockMvcResultMatchers.jsonPath("$[*].stockCode", CoreMatchers.everyItem(CoreMatchers.is("066570"))));
+		responseOfAllPurchaseRecordOnPeriod.andExpect(MockMvcResultMatchers.jsonPath("$[*].stockName", CoreMatchers.everyItem(CoreMatchers.is("LG전자"))));
+		responseOfAllPurchaseRecordOnPeriod.andExpect(MockMvcResultMatchers.jsonPath("$[*].dt", CoreMatchers.everyItem(CoreMatchers.is("2024-01-03"))));
+		responseOfAllPurchaseRecordOnPeriod.andExpect(
+				MockMvcResultMatchers.jsonPath("$[*].stockPrice",
+						CoreMatchers.everyItem(
+								CoreMatchers.anyOf(CoreMatchers.is(67100),CoreMatchers.is(68500),CoreMatchers.is(65100),CoreMatchers.is(68000))
+						)
+				)
+		);
+	}
 }
