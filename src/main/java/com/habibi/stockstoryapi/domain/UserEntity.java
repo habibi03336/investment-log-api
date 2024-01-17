1ad8632b416a8f442f08f1f9d0aadcafff81002b
하지훈 <jnh03336@gmail.com>
2024-01-17 22:43:42 +0700

    feat:  user entity implements user details for security

--- src/main/java/com/habibi/stockstoryapi/domain/UserEntity.java
+++ src/main/java/com/habibi/stockstoryapi/domain/UserEntity.java
@@ -4,14 +4,48 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of = "userId")
 @Entity
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id @GeneratedValue
     private int userId;
     private String email;
     private String password;
     private String name;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
