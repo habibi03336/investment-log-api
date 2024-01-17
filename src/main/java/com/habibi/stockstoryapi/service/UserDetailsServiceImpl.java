a9fc7c4ec97e3bad9e019adc56ad7e73a1987da0
하지훈 <jnh03336@gmail.com>
2024-01-17 22:44:29 +0700

    feat: implement user detail service interface

--- src/main/java/com/habibi/stockstoryapi/service/UserDetailsServiceImpl.java
+++ src/main/java/com/habibi/stockstoryapi/service/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.habibi.stockstoryapi.service;
 
+import com.habibi.stockstoryapi.repository.UserRepository;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,26 +12,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
-
-    private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
-            new User(
-                    "jnh03336@gmail.com",
-                    "password",
-                    Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))
-            ),
-            new User(
-                    "user03336@gmail.com",
-                    "password",
-                    Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
-            )
-    );
+    private UserRepository userRepository;
+    public UserDetailsServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return APPLICATION_USERS
-                .stream()
-                .filter(u -> u.getUsername().equals(email))
-                .findFirst()
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return userRepository.findByEmail(email);
     }
 }
