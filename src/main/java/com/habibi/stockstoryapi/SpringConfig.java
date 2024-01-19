e03e58d8a7049e6a21f8d9443758e4e11a58e5d7
하지훈 <jnh03336@gmail.com>
2024-01-19 18:49:35 +0700

    feat: spring security, enable method security
    - controll authorization with prepost annotations

--- src/main/java/com/habibi/stockstoryapi/SpringConfig.java
+++ src/main/java/com/habibi/stockstoryapi/SpringConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -34,6 +34,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
         @PropertySource("classpath:.env")
 })
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SpringConfig {
     private final StockPositionStoryRepository stockPositionStoryRepository;
     private final StockPurchaseRecordRepository stockPurchaseRecordRepository;
