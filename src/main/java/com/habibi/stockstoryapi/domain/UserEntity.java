567dd0b2e3c3c166c0f0a7bff98e60eea71185a8
하지훈 <jnh03336@gmail.com>
2024-01-19 18:53:21 +0700

    feat: add default 'USER' role for all users

--- src/main/java/com/habibi/stockstoryapi/domain/UserEntity.java
+++ src/main/java/com/habibi/stockstoryapi/domain/UserEntity.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of = "userId")
@@ -21,7 +22,12 @@ public class UserEntity implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return "USER";
+            }
+        });
     }
 
     @Override
