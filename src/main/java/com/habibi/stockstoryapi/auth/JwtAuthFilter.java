73f098b6142ce95c753f47657f08b52e5ef8c25d
하지훈 <jnh03336@gmail.com>
2024-01-20 16:49:21 +0700

    fix: typo

--- src/main/java/com/habibi/stockstoryapi/auth/JwtAuthFilter.java
+++ src/main/java/com/habibi/stockstoryapi/auth/JwtAuthFilter.java
@@ -1,6 +1,6 @@
 package com.habibi.stockstoryapi.auth;
 
-`import com.habibi.stockstoryapi.dto.UserDetailsDto;
+import com.habibi.stockstoryapi.dto.UserDetailsDto;
 import com.habibi.stockstoryapi.service.UserDetailsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
