7060373399542383920fad0fa953b49207dacb61
하지훈 <jnh03336@gmail.com>
2024-01-20 08:46:26 +0700

    feat: implement custom user detail serivce

--- src/main/java/com/habibi/stockstoryapi/service/UserDetailsServiceImpl.java
+++ src/main/java/com/habibi/stockstoryapi/service/UserDetailsServiceImpl.java
@@ -1,15 +1,9 @@
 package com.habibi.stockstoryapi.service;
 
+import com.habibi.stockstoryapi.domain.UserEntity;
+import com.habibi.stockstoryapi.dto.UserDetailsDto;
 import com.habibi.stockstoryapi.repository.UserRepository;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
     private UserRepository userRepository;
@@ -18,7 +12,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email);
+    public UserDetailsDto loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user =  userRepository.findByEmail(email);
+        return new UserDetailsDto(user.getUserId(), user.getEmail(), user.getPassword());
     }
 }
