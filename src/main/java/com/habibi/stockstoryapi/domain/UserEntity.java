9be024bf88cb6192d35e69900f96dd4afd1f5cc4
하지훈 <jnh03336@gmail.com>
2024-01-20 08:44:33 +0700

    feat: create user details dto (seperate from entity)

--- src/main/java/com/habibi/stockstoryapi/domain/UserEntity.java
+++ src/main/java/com/habibi/stockstoryapi/domain/UserEntity.java
@@ -4,54 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.List;
 
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of = "userId")
 @Entity
-public class UserEntity implements UserDetails {
+public class UserEntity {
     @Id @GeneratedValue
     private int userId;
     private String email;
     private String password;
     private String name;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new GrantedAuthority() {
-            @Override
-            public String getAuthority() {
-                return "USER";
-            }
-        });
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
---
+++ src/main/java/com/habibi/stockstoryapi/dto/UserDetailsDto.java
@@ -0,0 +1,64 @@
+package com.habibi.stockstoryapi.dto;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailsDto implements UserDetails {
+
+    private int userId;
+    private String email;
+    private String password;
+
+    public UserDetailsDto(int userId, String email, String password){
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return "USER";
+            }
+        });
+    }
+
+    public int getUserId(){
+        return userId;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
